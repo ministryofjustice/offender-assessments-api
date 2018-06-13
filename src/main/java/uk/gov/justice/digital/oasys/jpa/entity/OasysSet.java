19261e770ff0ddc6f68c5d9b3d84fbc55aa7cecc
AntonyGowland <tonyuk28@googlemail.com>
2018-06-13 15:32:15 +0100

    Add Controller test

--- src/main/java/uk/gov/justice/digital/oasys/jpa/entity/OasysSet.java
+++ src/main/java/uk/gov/justice/digital/oasys/jpa/entity/OasysSet.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.oasys.jpa.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 import java.sql.Time;
 
 @Data
+@Builder
 @Entity
 @Table(name = "OASYS_SET")
 public class OasysSet {
