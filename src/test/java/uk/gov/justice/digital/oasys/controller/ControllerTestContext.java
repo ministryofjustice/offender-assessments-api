6149e02507849398c24cdd27348fd553ea68bfe0
AntonyGowland <tonyuk28@googlemail.com>
2018-06-27 11:11:05 +0100

    WIP

---
+++ src/test/java/uk/gov/justice/digital/oasys/controller/ControllerTestContext.java
@@ -0,0 +1,125 @@
+package uk.gov.justice.digital.oasys.controller;
+
+import com.google.common.collect.ImmutableList;
+import org.mockito.Mockito;
+import uk.gov.justice.digital.oasys.jpa.entity.OasysAssessmentGroup;
+import uk.gov.justice.digital.oasys.jpa.entity.OasysSet;
+import uk.gov.justice.digital.oasys.jpa.entity.Offender;
+import uk.gov.justice.digital.oasys.jpa.entity.RefElement;
+import uk.gov.justice.digital.oasys.jpa.repository.OffenderRepository;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Matchers.eq;
+
+public class ControllerTestContext {
+
+    public static void setup(OffenderRepository offenderRepository) {
+
+        Mockito.when(offenderRepository.findByCmsProbNumber(eq("crn1"))).thenReturn(anOffender());
+        Mockito.when(offenderRepository.findByCmsProbNumber(eq("crn2"))).thenReturn(Optional.empty());
+        Mockito.when(offenderRepository.findByCmsPrisNumber(eq("nomisId1"))).thenReturn(anOffender());
+        Mockito.when(offenderRepository.findByCmsPrisNumber(eq("nomisId2"))).thenReturn(Optional.empty());
+        Mockito.when(offenderRepository.findByPrisonNumber(eq("bookingId1"))).thenReturn(anOffender());
+        Mockito.when(offenderRepository.findByPrisonNumber(eq("bookingId2"))).thenReturn(Optional.empty());
+        Mockito.when(offenderRepository.findByPnc(eq("pnc1"))).thenReturn(anOffender());
+        Mockito.when(offenderRepository.findByPnc(eq("pnc2"))).thenReturn(Optional.empty());
+        Mockito.when(offenderRepository.findOne(eq(1L))).thenReturn(anOffender().get());
+        Mockito.when(offenderRepository.findOne(eq(2L))).thenReturn(null);
+
+    }
+
+    private static Optional<Offender> anOffender() {
+        return Optional.ofNullable(Offender.builder()
+                .oasysAssessmentGroups(anAssessmentGroup())
+                .build());
+    }
+
+    private static List<OasysAssessmentGroup> anAssessmentGroup() {
+        return ImmutableList.of(OasysAssessmentGroup.builder()
+                .oasysAssessmentGroupPk(1L)
+                .oasysSets(someOasysSets())
+                .build());
+    }
+
+    private static List<OasysSet> someOasysSets() {
+        return ImmutableList.of(OasysSet.builder()
+                        .createDate(new Timestamp(System.currentTimeMillis() - oneDay()))
+                        .assessmentType(assessmentType("oasys"))
+                        .oasysSetPk(1L)
+                        .ogrs31Year(BigDecimal.ONE)
+                        .ogrs32Year(BigDecimal.TEN)
+                        .ogp1Year(BigDecimal.ONE)
+                        .ogp2Year(BigDecimal.TEN)
+                        .ogpDyWesc(BigDecimal.ONE)
+                        .ogpStWesc(BigDecimal.TEN)
+                        .ogpTotWesc(BigDecimal.ONE)
+                        .ovpStWesc(BigDecimal.TEN)
+                        .ovpDyWesc(BigDecimal.ONE)
+                        .ovpTotWesc(BigDecimal.TEN)
+                        .ovp1Year(BigDecimal.ONE)
+                        .ovp2Year(BigDecimal.TEN)
+                        .ovpPrevWesc(BigDecimal.ONE)
+                        .ovpVioWesc(BigDecimal.TEN)
+                        .ovpNonVioWesc(BigDecimal.ONE)
+                        .ovpAgeWesc(BigDecimal.TEN)
+                        .ovpSexWesc(BigDecimal.ONE)
+                        .group(aGroup("HISTORIC"))
+                        .assessmentStatus(anAssessmentStatus("OPEN"))
+                        .build(),
+                OasysSet.builder()
+                        .createDate(new Timestamp(System.currentTimeMillis()))
+                        .assessmentType(assessmentType("sara"))
+                        .oasysSetPk(2L)
+                        .ogrs31Year(BigDecimal.TEN)
+                        .ogrs32Year(BigDecimal.ONE)
+                        .ogp1Year(BigDecimal.TEN)
+                        .ogp2Year(BigDecimal.ONE)
+                        .ogpDyWesc(BigDecimal.TEN)
+                        .ogpStWesc(BigDecimal.ONE)
+                        .ogpTotWesc(BigDecimal.TEN)
+                        .ovpStWesc(BigDecimal.ONE)
+                        .ovpDyWesc(BigDecimal.TEN)
+                        .ovpTotWesc(BigDecimal.ONE)
+                        .ovp1Year(BigDecimal.TEN)
+                        .ovp2Year(BigDecimal.ONE)
+                        .ovpPrevWesc(BigDecimal.TEN)
+                        .ovpVioWesc(BigDecimal.ONE)
+                        .ovpNonVioWesc(BigDecimal.TEN)
+                        .ovpAgeWesc(BigDecimal.ONE)
+                        .ovpSexWesc(BigDecimal.TEN)
+                        .group(aGroup("CURRENT"))
+                        .assessmentStatus(anAssessmentStatus("COMPLETE"))
+                        .assessmentVoidedDate(new Timestamp(System.currentTimeMillis()))
+                        .build());
+    }
+
+    private static RefElement anAssessmentStatus(String status) {
+        return RefElement.builder().refElementCode(status).build();
+    }
+
+    private static OasysAssessmentGroup aGroup(String status) {
+        return OasysAssessmentGroup.builder()
+                .historicStatusELm(status)
+                .build();
+    }
+
+    private static long oneDay() {
+        return 1000 * 60 * 60 * 24;
+    }
+    private static RefElement assessmentType(String type) {
+        return RefElement.builder().refElementCode(type).build();
+    }
+
+    public static OasysSet anOasysSet(Long id) {
+        return OasysSet.builder()
+                .assessmentType(RefElement.builder().build())
+                .group(OasysAssessmentGroup.builder().build())
+                .assessmentStatus(RefElement.builder().build())
+                .oasysSetPk(id).build();
+    }
+
+}
--- src/test/java/uk/gov/justice/digital/oasys/controller/OvpControllerTest.java
+++ src/test/java/uk/gov/justice/digital/oasys/controller/OvpControllerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.oasys.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableList;
 import io.restassured.RestAssured;
 import io.restassured.config.ObjectMapperConfig;
 import io.restassured.config.RestAssuredConfig;
@@ -9,7 +8,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -18,16 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.justice.digital.oasys.api.Ogp;
-import uk.gov.justice.digital.oasys.jpa.entity.OasysAssessmentGroup;
-import uk.gov.justice.digital.oasys.jpa.entity.OasysSet;
-import uk.gov.justice.digital.oasys.jpa.entity.Offender;
-import uk.gov.justice.digital.oasys.jpa.entity.RefElement;
 import uk.gov.justice.digital.oasys.jpa.repository.OffenderRepository;
-
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -55,99 +44,8 @@ public class OvpControllerTest {
         RestAssured.config = RestAssuredConfig.config().objectMapperConfig(new ObjectMapperConfig().jackson2ObjectMapperFactory(
                 (aClass, s) -> objectMapper
         ));
-
-        Mockito.when(offenderRepository.findByCmsProbNumber(eq("crn1"))).thenReturn(anOffender());
-        Mockito.when(offenderRepository.findByCmsProbNumber(eq("crn2"))).thenReturn(Optional.empty());
-        Mockito.when(offenderRepository.findByCmsPrisNumber(eq("nomisId1"))).thenReturn(anOffender());
-        Mockito.when(offenderRepository.findByCmsPrisNumber(eq("nomisId2"))).thenReturn(Optional.empty());
-        Mockito.when(offenderRepository.findByPrisonNumber(eq("bookingId1"))).thenReturn(anOffender());
-        Mockito.when(offenderRepository.findByPrisonNumber(eq("bookingId2"))).thenReturn(Optional.empty());
-        Mockito.when(offenderRepository.findByPnc(eq("pnc1"))).thenReturn(anOffender());
-        Mockito.when(offenderRepository.findByPnc(eq("pnc2"))).thenReturn(Optional.empty());
-        Mockito.when(offenderRepository.findOne(eq(1L))).thenReturn(anOffender().get());
-        Mockito.when(offenderRepository.findOne(eq(2L))).thenReturn(null);
-
+        ControllerTestContext.setup(offenderRepository);
     }
-
-    private Optional<Offender> anOffender() {
-        return Optional.ofNullable(Offender.builder()
-                .oasysAssessmentGroups(anAssessmentGroup())
-                .build());
-    }
-
-    private List<OasysAssessmentGroup> anAssessmentGroup() {
-        return ImmutableList.of(OasysAssessmentGroup.builder()
-                .oasysAssessmentGroupPk(1L)
-                .oasysSets(someOasysSets())
-                .build());
-    }
-
-    private List<OasysSet> someOasysSets() {
-        return ImmutableList.of(OasysSet.builder()
-                        .createDate(new Timestamp(System.currentTimeMillis()))
-                        .assessmentType(assessmentType("oasys"))
-                        .oasysSetPk(1L)
-                        .ogrs31Year(BigDecimal.ONE)
-                        .ogrs32Year(BigDecimal.TEN)
-                        .ogp1Year(BigDecimal.ONE)
-                        .ogp2Year(BigDecimal.TEN)
-                        .ogpDyWesc(BigDecimal.ONE)
-                        .ogpStWesc(BigDecimal.TEN)
-                        .ogpTotWesc(BigDecimal.ONE)
-                        .ovpStWesc(BigDecimal.TEN)
-                        .ovpDyWesc(BigDecimal.ONE)
-                        .ovpTotWesc(BigDecimal.TEN)
-                        .ovp1Year(BigDecimal.ONE)
-                        .ovp2Year(BigDecimal.TEN)
-                        .ovpPrevWesc(BigDecimal.ONE)
-                        .ovpVioWesc(BigDecimal.TEN)
-                        .ovpNonVioWesc(BigDecimal.ONE)
-                        .ovpAgeWesc(BigDecimal.TEN)
-                        .ovpSexWesc(BigDecimal.ONE)
-                        .group(aGroup("HISTORIC"))
-                        .assessmentStatus(anAssessmentStatus("OPEN"))
-                        .build(),
-                OasysSet.builder()
-                        .createDate(new Timestamp(System.currentTimeMillis()))
-                        .assessmentType(assessmentType("sara"))
-                        .oasysSetPk(2L)
-                        .ogrs31Year(BigDecimal.TEN)
-                        .ogrs32Year(BigDecimal.ONE)
-                        .ogp1Year(BigDecimal.TEN)
-                        .ogp2Year(BigDecimal.ONE)
-                        .ogpDyWesc(BigDecimal.TEN)
-                        .ogpStWesc(BigDecimal.ONE)
-                        .ogpTotWesc(BigDecimal.TEN)
-                        .ovpStWesc(BigDecimal.ONE)
-                        .ovpDyWesc(BigDecimal.TEN)
-                        .ovpTotWesc(BigDecimal.ONE)
-                        .ovp1Year(BigDecimal.TEN)
-                        .ovp2Year(BigDecimal.ONE)
-                        .ovpPrevWesc(BigDecimal.TEN)
-                        .ovpVioWesc(BigDecimal.ONE)
-                        .ovpNonVioWesc(BigDecimal.TEN)
-                        .ovpAgeWesc(BigDecimal.ONE)
-                        .ovpSexWesc(BigDecimal.TEN)
-                        .group(aGroup("CURRENT"))
-                        .assessmentStatus(anAssessmentStatus("COMPLETE"))
-                        .assessmentVoidedDate(new Timestamp(System.currentTimeMillis()))
-                        .build());
-    }
-
-    private RefElement anAssessmentStatus(String status) {
-        return RefElement.builder().refElementCode(status).build();
-    }
-
-    private OasysAssessmentGroup aGroup(String status) {
-        return OasysAssessmentGroup.builder()
-                .historicStatusELm(status)
-                .build();
-    }
-
-    private RefElement assessmentType(String type) {
-        return RefElement.builder().refElementCode(type).build();
-    }
-
     @After
     public void tearDown() {
     }
